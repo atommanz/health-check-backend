08fb2374b2a248969d5a71c791dec7c3abb2ff0e
atomercc <atomerns@gmail.com>
2020-03-13 15:08:48 +0700

    [atom] clean code

--- services/request.js
+++ services/request.js
@@ -2,7 +2,6 @@
 const request = require('request')
 
 const check = function (url) {
-    // console.log('check', url)
     const start = new Date()
     return new Promise(function (resolve, reject) {
 
@@ -17,31 +16,29 @@ const check = function (url) {
             const durationMilli = end - start
 
             const durationNano = durationMilli * 1000000
-            // console.log('start', start, 'end', end, 'duration', durationNano)
             if (error) {
-                // console.log('Err: ' + error);
-                return reject({ ...out, alive: false, durationNano });
+                 const end = new Date()
+                const durationMilli = end - start
+
+                const durationNano = durationMilli * 1000000
+                return resolve({ ...out, alive: false, durationNano });
             }
 
             if (response.statusCode == 200 || response.statusCode == 201 || response.statusCode == 202) {
-                // console.log(link + ' is up!!');
-                return resolve({ ...out, alive: true, durationNano });
-                // return false;
+                 return resolve({ ...out, alive: true, durationNano });
+               
             }
 
             if (response.statusCode == 301 || response.statusCode == 302) {
-                // console.log(link + ' is redirecting us!!');
                 return resolve({ ...out, alive: true, durationNano });
-                // return false;
+               
             }
 
             if (response.statusCode == 401) {
-                // console.log("you are unauthorized to " + link);
-                return resolve({ ...out, alive: true, durationNano });
-                // return false;
+               return resolve({ ...out, alive: true, durationNano });
+              
             } else {
-                // console.log(link + ' is down!!');
-                reject({ ...out, alive: false, durationNano });
+                resolve({ ...out, alive: false, durationNano });
             }
 
         })
